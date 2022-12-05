a1d23854bb94ef06ee2715b1ecc7bf4dd78a6e6d
An3j <119878964+An3j@users.noreply.github.com>
2022-12-05 17:05:30 +0100

    Add files via upload

---
+++ vaja6.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+mkdir Desktop Documents Downloads Pictures Videos;
+
+for i in {1..5}
+do 
+mkdir "folder$i"
+sudo useradd -m --groups sudo user$i
+done
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install ufw
+sudo apt install git
+sudo apt install nginx
+sudo apt install net-tools
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  sudo docker run hello-world
+  
